c6690ab961803359730f5630b0bd13169e3ac9e4
Felipe Freitas <felipe96.ff@gmail.com>
2022-07-28 21:09:02 -0300

    finalizado testes

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,14 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+    try {
+      const updatedUser = this.turnUserAdminUseCase.execute({ user_id });
+
+      return response.json(updatedUser).send();
+    } catch (e) {
+      return response.status(404).json({ error: e.message });
+    }
   }
 }
 
